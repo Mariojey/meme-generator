97513cc1ffdc6ec2e79592b0fe96706948c8eb71
Mariusz Jacek <marios.yaceg@gmail.com>
2022-08-24 18:29:42 +0200

    init

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <header className="header">
+            <img src="./images/troll-face.png" alt="troll face" className="header--image"/>
+            <h2 className="header-title">Meme Generator</h2>
+            <h4 className="header--project">Reactr - Course 2</h4>
+        </header>
+    )
+}
